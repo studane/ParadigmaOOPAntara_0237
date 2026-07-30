e66788778ff061377c02039b448292271bd0f6c4
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-30 14:38:47 +0700

    mengisi variabel dan membuat prosedur

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -3,4 +3,14 @@ using namespace std;
 
 class Mahasiswa{
     public :
-}
+    int nim;
+    string nama;
+    float nilai;
+
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+}; //batas class
+
